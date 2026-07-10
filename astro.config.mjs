b66b5ad19b6a1ecea6ab262db0d5d0d3bd4fb798
Jose Armando <samja1702ap@gmail.com>
2026-07-10 10:01:38 -0600

    landing

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	// TODO: reemplaza con el dominio real donde vivirá esta landing (o el subdominio que uses)
+	site: 'https://alalibreagency.com',
+});
